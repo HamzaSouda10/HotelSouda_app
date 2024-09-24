099a417b4876c17adde1f93f9813d226b913ab31
HamzaSouda10 <hamzasouda06@gmail.com>
2024-09-24 23:28:34 +0100

    first day

---
+++ Souda_Hotel/Souda_Hotel/src/main/java/com/Souda/Souda_Hotel/SoudaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.Souda.Souda_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoudaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoudaHotelApplication.class, args);
+	}
+
+}
